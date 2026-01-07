738b4f5c37e0d8ae6a1eec2b501fdee600fbd419
mohamedshemees <mohamedessampd@gmail.com>
2026-01-07 02:28:11 +0200

    Add app color palette

---
+++ lib/presentation/design_system/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+  static const  primary=Color(0xFFFFA451);
+  static const  secondary=Color(0xFFF08626);
+  static const  violet=Color(0xFF938DB5);
+  static const  darkBlue=Color(0xFF27214D);
+  static const  hint=Color(0xFFC2BDBD);
+  static const  pureWhite=Color(0xFFFFFFFF);
+  static const  pureBlack=Color(0xFF000000);
+}
