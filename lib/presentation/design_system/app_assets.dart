5fbdef968113e4739a0e4fd0b71b3fadf2b68dd7
mohamedshemees <mohamedessampd@gmail.com>
2026-01-07 03:40:31 +0200

    Add app strings and assets as const

---
+++ lib/presentation/design_system/app_assets.dart
@@ -0,0 +1,22 @@
+class AppAssets {
+  static const String icPlus = 'assets/ic_plus.svg';
+  static const String icTick = 'assets/ic_tick.svg';
+  static const String group58 = 'assets/img_app_logo.png';
+  static const String icClear = 'assets/ic_clear.svg';
+  static const String icMinus = 'assets/ic_minus.svg';
+  static const String icPhone = 'assets/ic_phone.svg';
+  static const String icFilter = 'assets/ic_filter.svg';
+  static const String icCurrency = 'assets/ic_currency.svg';
+  static const String icFavorite = 'assets/ic_favorite.svg';
+  static const String icOrderTaken = 'assets/ic_order_taken.png';
+  static const String icChevronBack = 'assets/ic_chevron_back.svg';
+  static const String imgOrderAdded = 'assets/img_order_added.png';
+  static const String imgFruitBasket1 = 'assets/img_fruit_basket_1.png';
+  static const String imgFruitBasket2 = 'assets/img_fruit_basket_2.png';
+  static const String imgBreakfastQuinoa = 'assets/img_breakfast_quinoa.png';
+  static const String imgHoneyLimePeach = 'assets/img_honey_lime_peach.png';
+  static const String icOrderProcessed = 'assets/ic_order_processed.png.png';
+  static const String imgTropicalFruitSalad = 'assets/img_tropical_fruit_salad.png';
+  static const String imgFruitDrop = 'assets/img_fruit_drop.png';
+  static const String icDeliveryMan = 'assets/ic_delivery_man.png';
+}
---
+++ lib/presentation/design_system/app_strings.dart
@@ -0,0 +1,72 @@
+class AppStrings {
+  // Onboarding / Welcome
+  static const String getTheFreshestFruitSaladCombo = 'Get The Freshest Fruit Salad Combo';
+  static const String weDeliverTheBest = 'We deliver the best and freshest fruit salad in town. Order for a combo today!!!';
+  static const String whatIsYourFirstname = 'What is your firstname?';
+  static const String letsContinue = 'Let’s Continue';
+  static const String startOrdering = 'Start Ordering';
+
+  // Home Screen
+  static const String homeWelcome = 'Hello %s, What fruit salad combo do you want today?';
+  static const String myBasket = 'My basket';
+  static const String searchForFruitSaladCombos = 'Search for fruit salad combos';
+  static const String recommendedCombo = 'Recommended Combo';
+  static const String honeyLimeCombo = 'Honey lime combo';
+  static const String berryMangoCombo = 'Berry mango combo';
+
+  // Categories/Tags
+  static const String hottest = 'Hottest';
+  static const String popular = 'Popular';
+  static const String newCombo = 'New combo';
+  static const String top = 'Top';
+
+  // Meal Names
+  static const String quinoaFruitSalad = 'Quinoa fruit salad';
+  static const String tropicalFruitSalad = 'Tropical fruit salad';
+  static const String melonFruitSalad = 'melon fruit salad';
+
+  // Meal Details
+  static const String quinoaFruitSaladTitle = 'Quinoa Fruit Salad';
+  static const String onePackContains = 'One Pack Contains:';
+  static const String quinoaFruitSaladIngredients = 'Red Quinoa, Lime, Honey, Blueberries, Strawberries, Mango, Fresh mint.';
+  static const String quinoaFruitSaladDescription = 'If you are looking for a new fruit salad to eat today, quinoa is the perfect brunch for you. make';
+  static const String addToBasket = 'Add to basket';
+  static const String goBack = 'Go back';
+
+  // Checkout / Payment
+  static const String cardHoldersName = 'Card Holders Name';
+  static const String cardNumber = 'Card Number';
+  static const String date = 'Date';
+  static const String ccv = 'CCV';
+  static const String completeOrder = 'Complete Order';
+  static const String checkout = 'Checkout';
+  static const String total = 'Total';
+  static const String packs = 'packs';
+  static const String payOnDelivery = 'Pay on delivery';
+  static const String payWithCard = 'Pay with card';
+  static const String myBasketTitle = 'My Basket';
+
+  // Order Status
+  static const String deliveryStatus = 'Delivery Status';
+  static const String orderTaken = 'Order Taken';
+  static const String orderIsBeingPrepared = 'Order Is Being Prepared';
+  static const String orderIsBeingDelivered = 'Order Is Being Delivered';
+  static const String yourDeliveryAgentIsComing = 'Your delivery agent is coming';
+  static const String orderReceived = 'Order Received';
+  static const String congratulations = 'Congratulations!!!';
+  static const String yourOrderHasBeenTaken = 'Your order have been taken and is being attended to';
+  static const String trackOrder = 'Track order';
+  static const String continueShopping = 'Continue shopping';
+
+  // Delivery Details
+  static const String deliveryAddress = 'Delivery address';
+  static const String numberWeCanCall = 'Number we can call';
+
+  // Example Data (You might want to remove these from a final app)
+  static const String hintCardHolderName = 'Adolphus Chris';
+  static const String hintCardNumber = '1234 5678 9012 1314';
+  static const String hintExpiryDate = '10/30';
+  static const String hintCcv = '123';
+  static const String hintDeliveryAddress = '10th avenue, Lekki, Lagos State';
+  static const String hintPhoneNumber = '09090605708';
+}
