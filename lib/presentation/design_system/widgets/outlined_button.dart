d2dd8568469b412f7bda624b4e8717a7fb17aa14
mohamedshemees <mohamedessampd@gmail.com>
2026-01-09 14:37:44 +0200

    Wrap buttons with IntrinsicWidth widget

--- lib/presentation/design_system/widgets/outlined_button.dart
+++ lib/presentation/design_system/widgets/outlined_button.dart
@@ -11,24 +11,26 @@ class CustomOutlinedButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 16,horizontal:16 ),
-        decoration: BoxDecoration(
-          color: AppColors.pureWhite,
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(color: AppColors.primary, width: 1),
-        ),
-        alignment: Alignment.center,
-        child: Text(
-          text,
-          style: const TextStyle(
-            fontFamily: AppFonts.brandonGrotesque,
-            color: AppColors.primary,
-            fontSize: 16,
-            height: 1.5,
-            fontWeight: FontWeight.w500,
+    return IntrinsicWidth(
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 16,horizontal:16 ),
+          decoration: BoxDecoration(
+            color: AppColors.pureWhite,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: AppColors.primary, width: 1),
+          ),
+          alignment: Alignment.center,
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontFamily: AppFonts.brandonGrotesque,
+              color: AppColors.primary,
+              fontSize: 16,
+              height: 1.5,
+              fontWeight: FontWeight.w500,
+            ),
           ),
         ),
       ),
