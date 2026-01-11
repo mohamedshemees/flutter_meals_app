56a2c4f724fac9c0a3337ed7f6b236ffde3acf47
marah222 <maraharam222@gmail.com>
2026-01-11 13:21:25 +0300

    feat: add meal Business logic

--- lib/di/dependency_provider.dart
+++ lib/di/dependency_provider.dart
@@ -1,11 +1,21 @@
+import 'package:food_store_flutter/data/datasources/meals_local_datasource.dart';
+import 'package:food_store_flutter/data/repositories/meals_repository_impl.dart';
+import 'package:food_store_flutter/domain/repository/meals_repository.dart';
+import 'package:food_store_flutter/presentation/screens/home/home_screen_cubit.dart';
 import 'package:get_it/get_it.dart';
 
-import '../domain/repository/cart_repository.dart';
-
 final getIt = GetIt.instance;
+
 Future<void> configureDependencies() async {
-  //e.g how to provide an object with di
-  // getIt.registerLazySingleton<CartRepository>(
-  //       () => CartRepositoryImpl(),
-  // );
+  getIt.registerLazySingleton<MealsLocalDataSource>(
+    () => MealsLocalDataSource(),
+  );
+
+  getIt.registerLazySingleton<MealsRepository>(
+    () => MealsRepositoryImpl(getIt<MealsLocalDataSource>()),
+  );
+
+  getIt.registerFactory<HomeScreenCubit>(
+    () => HomeScreenCubit(getIt<MealsRepository>()),
+  );
 }
